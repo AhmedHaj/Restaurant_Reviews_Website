92ee86aa3bd339fee7c5942c7355c8a5dc468c15
Jonathan Calles <jcall057@uottawa.ca>
2018-03-15 23:04:29 -0400

    Created a folder to test code and functions

---
+++ TestCode_Jonathan/handle_search.php
@@ -0,0 +1,25 @@
+<!-- Currently this php code, as it stands, will generate a new webpage for easy testing. However this code could be function directly within index.html -->
+
+<!doctype html>
+<html>
+	<head>
+		<title>Search Query Feedback</title>
+	</head>
+
+	<body>
+		<?php #this php code handles input data from the search form on index.html
+
+		//Create a shorthand for the data in the form, i.e. variables we can use.
+		$searchTermA = $_REQUEST['searchTermA'];
+		$searchTermB = $_REQUEST['searchTermB'];
+		$searchTermC = $_REQUEST['searchTermC'];
+
+		//Display the submitted information using the created variables inside an echo command
+		echo "<p> Search query terms received</p><p>You entered $searchTermA $searchTermB $searchTermC </p>";
+		?>
+
+
+	</body>
+	
+
+</html>
